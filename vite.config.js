9d2120bc551f67199f1198b1c8a0cfcfefd00581
UnaRem <58946779+UnaRem@users.noreply.github.com>
2025-06-17 13:15:41 +0800

    Update vite.config.js
    production

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  base:process.env.NODE_ENV === 'production'
+  ? '/buffet/'
+    :'/'
 })
